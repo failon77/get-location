538cc20dc2b30803ae85d142af2fda68ef376382
Hani <89615778+failon77@users.noreply.github.com>
2022-07-18 10:25:50 +0300

    Update get location.py

--- get location.py
+++ get location.py
@@ -1,7 +1,7 @@
 import geocoder
 import folium
 
-ip = geocoder.ip("36.175, -115.1372")
+ip = geocoder.ip("me")
 location = ip.latlng
 map = folium.Map(location=location, zoom_start=10)
 folium.Marker(location).add_to(map)
